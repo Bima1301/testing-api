b9693307f605ae22e4fca0e5b5aa1cbac5ce94a2
Bima1301 <bima.aji1380@gmail.com>
2024-05-11 21:15:52 +0700

    minor

--- app/Http/Middleware/TrustHosts.php
+++ app/Http/Middleware/TrustHosts.php
@@ -15,7 +15,7 @@ class TrustHosts extends Middleware
     {
         return [
             $this->allSubdomainsOfApplicationUrl(),
-            'http://localhost:5173/'
+            'http://localhost:5173'
         ];
     }
 }
