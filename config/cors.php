2d8c72a48b13aff6b6e8634aa97c290c8996115d
Bima1301 <bima.aji1380@gmail.com>
2024-05-11 21:21:39 +0700

    minor

--- config/cors.php
+++ config/cors.php
@@ -15,20 +15,20 @@ return [
     |
     */
 
-    'paths' => ['*'],
+    'paths' => ['api/products', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173/'],
+    'allowed_origins' => ['http://localhost:5173'],
 
-    'allowed_origins_patterns' => ['*'],
+    'allowed_origins_patterns' => [],
 
     'allowed_headers' => ['*'],
 
-    'exposed_headers' => ['*'],
+    'exposed_headers' => [],
 
     'max_age' => 0,
 
-    'supports_credentials' => false,
+    'supports_credentials' => true,
 
 ];
